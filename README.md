63d45489f11b0810b42b6ec4dd1f47f0be0cb61a
Pouria Amini <168713477+MrAminiDev@users.noreply.github.com>
2025-11-10 09:42:12 +0330

    Update and rename README.md to install.sh

--- README.md
+++
@@ -1,2 +0,0 @@
-# Nexo-SelfBot
-🤖 Nexo Selfie is a selfie-making bot and intelligent virtual assistant on Telegram designed to improve and simplify your daily tasks. With over 100 useful tools, Nexo allows you to perform a variety of tasks quickly and without the need for technical knowledge.
---
+++ install.sh
@@ -0,0 +1,147 @@
+<div align="center"><img src="https://raw.githubusercontent.com/MrAminiDev/Nexo-SelfBot/main/Nexo.PNG" width="500"></div><br>
+<div align="center">
+برای توضیحات <a href="https://github.com/MrAminiNezhad/Nexo-SelfBot/blob/main/README-fa.md"> فارسی اینجا بزنید </a>
+
+</div>
+<br><br>
+
+
+# Nexo-SelfBot
+🤖 Nexo Selfie is a selfie-making bot and intelligent virtual assistant on Telegram designed to improve and simplify your daily tasks. With over 100 useful tools, Nexo allows you to perform a variety of tasks quickly and without the need for technical knowledge.
+
+# learn inistallation
+
+1- For quick installation, type the following command to install the script prerequisites.<br>
+```
+  bash <(curl -Ls https://raw.githubusercontent.com/MrAminiDev/Nexo-SelfBot/main/install.sh)
+```
+
+2- Then edit the contents of the file with the command below ( Line  20 ~ 22 )
+```
+nano main.py
+```
+3- To run the script, press <br>
+```
+nohup python3 main.py &
+```
+4- You can use the following command to turn off the robot
+```
+pkill -f "main.py"
+```
+
+# features 
+
+🎯 Basic and Media Commands (Part 1)<br>
+──────────────────────────────────────<br>
+
+1- Display channel ID information<br>
+2- Translate text with Replay<br>
+3- Rename song or file (new name must be written with format)<br>
+4- Display weather conditions<br>
+5- Convert sticker to photo<br>
+6- Convert photo to sticker<br>
+7- Convert movie to gif<br>
+8- Convert video or music to voice<br>
+9- Get the last 1500 chats in file format<br>
+10- Set nickname for a person<br>
+11- Create group<br>
+12- Create channel<br>
+13- Add image to profile<br>
+14- Get time and date<br>
+15- Get and send contact list in file format (with ability to save directly to phone)<br>
+16- Test if self is online<br>
+17- Reload self<br>
+18- Set account name<br>
+19- Set account biography<br>
+20- Save music to the phone<br>
+21- Delete music from the phone<br>
+22- Download music from the phone<br>
+23- Show the list of saved music<br>
+24- Clear the list of saved music<br>
+25- Save video to the phone<br>
+26- Delete video from the phone<br>
+27- Download video from the phone<br>
+28- Show the list of saved videos<br>
+29- Clear the list of saved videos<br>
+30- Send text word by word<br>
+31- Set bank card number (with account holder name)<br>
+32- Get card number<br>
+33- Delete card number<br>
+34- Calculator (with support for + - * / operators)<br>
+35- Get age information<br>
+36- Get user information with Reply<br>
+37- Save contact with Reply<br>
+38- Share your number<br>
+39- Delete message with Reply<br>
+40- Forward message to PV<br>
+41- Get ID User with Reply or @Username<br>
+42- Get username via ID<br>
+43- Get your personal ID<br>
+44- Get group ID<br>
+45- Get your name<br>
+46- Get your number<br>
+
+────────────────────────────────────────────<br>
+💬 Message and typing commands<br>
+────────────────────────────────────────────<br>
+60- Show list of automatic messages<br>
+61- Clear set messages<br>
+62- Add text to automatic public message mode<br>
+63- Delete message from public mode<br>
+64- Show list of public messages<br>
+65- Clear all public messages<br>
+
+────────────────────────────────────────────<br>
+👥 Group commands<br>
+────────────────────────────────────────────<br>
+
+66- Get group information<br>
+67- Add user to group<br>
+68- Leave group<br>
+69- Get group link<br>
+70- Set group name<br>
+71- Clear all group messages<br>
+72- Delete all group gifs<br>
+73- Delete all group photos<br>
+74- Group<br>
+80- Delete any number of messages<br>
+81- Delete all messages from a user<br>
+82- Leave all groups<br>
+83- Filter a word in a group<br>
+84- Remove a word filter in a group<br>
+85- Show the list of filtered words<br>
+86- Clear the filter list<br>
+87- ​​Force a word in a group<br>
+88- Force delete a word in a group<br>
+89- Show the list of forced words<br>
+90- Clear the list of forced words<br>
+91- Mention all group members<br>
+92- Mention only admins<br>
+93- Mention only members<br>
+94- Mention only bots<br>
+95- Kick all group users<br>
+96- Add a bot to prevent adding another bot<br>
+97- Pin a message (with a desired time)<br>
+98- Unpin<br>
+99- Re-pin old messages<br>
+100- Kick a user with a reply<br>
+101- Mute a user with a reply<br>
+102- Unmute a user<br>
+103- Show the list of silent users<br>
+104- Clear the list Silents<br>
+105- Save timed media<br>
+────────────────────────────────────────────<br>
+🔥 Advanced commands<br>
+────────────────────────────────────────────<br>
+
+106- Notify when a person is online or offline (in saved messages)<br>
+107- Disable notification of a person's online status<br>
+108- Prevent login to the account (AntiLogin)<br>
+109- Disable login prevention mode<br>
+110- Auto-reply when offline with specified text<br>
+111- Disable auto-reply (secretary)<br>
+112- Convert message to QR with reply<br>
+113- Automatically send promotional message at desired schedule<br>
+114- Download user's story and save in saved message
+115- Enable automatic saving of timed media
+116- Disable automatic saving of timed media
